3dca1dea01528587f51fdaef1635200eed694744
simplon-mariyamH <mariyam.hamdaoui@gmail.com>
2017-02-20 09:02:05 +0100

    structure ajouté

---
+++ head.php
@@ -0,0 +1,3 @@
+<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="ecostyle.css">
+	<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.teal-green.min.css" />
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<?php include('head.php'); ?>
+<head>
+	<title>EcoStyle</title>
+</head>
+<body>
+	<?php include('menu.php'); ?>
+		  <main class="mdl-layout__content">
+	    <div class="page-content"><!-- Your content goes here --></div>
+	  </main>
+	</div>
+	<div></div>
+	<div></div>
+	<div></div>
+	<?php include('footer.php'); ?>
+</body>
+</html>
